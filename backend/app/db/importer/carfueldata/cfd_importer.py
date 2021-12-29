b8ad13b69f966c42a978244d9597d30383cf3f0c
Julian Psotta <julianpsotta@gmail.com>
2021-12-29 14:54:25 +0100

    add carfueldata import and reader objects

---
+++ backend/app/db/importer/carfueldata/cfd_importer.py
@@ -0,0 +1,36 @@
+from sqlalchemy.orm import Session
+from tqdm import tqdm
+
+from app.db.importer.base_importer import BaseImporter
+from app.db.importer.carfueldata.cfd_objects import CFDImportCar
+from app.db.importer.carfueldata.cfd_reader import CarFuelDataReader
+from app.models import CarFuelDataCar
+
+
+class CarFuelDataImporter(BaseImporter):
+    def __init__(self, db: Session):
+        super().__init__(db)
+        self.cfd_model_objects = []
+
+    def update_cfs_hashes(self, cfd_car_object: CarFuelDataCar):
+        hash_id = cfd_car_object.hash_id
+        db_object: CarFuelDataCar = self.db.query(CarFuelDataCar).get(hash_id)
+        db_object.upate(cfd_car_object)
+        self.db.commit()
+
+    def _store_cfd(self, cfd_car_import: CFDImportCar):
+        cfd_db_car: CarFuelDataCar = CarFuelDataCar()
+        cfd_db_car.translate_import(cfd_import_car=cfd_car_import)
+        self.cfd_model_objects.append(cfd_db_car)
+
+    def import_cfd_reader(self, cfd_reader: CarFuelDataReader):
+        for cfd_car_object in tqdm(
+            cfd_reader.cfd_objects_list,
+            total=len(cfd_reader.cfd_objects_list),
+            unit=" Importing CarFuelData",
+        ):
+            if type(cfd_car_object) == CFDImportCar:
+                self._store_cfd(cfd_car_object)
+            elif type(cfd_car_object) == CarFuelDataCar:
+                self.cfd_model_objects.append(cfd_car_object)
+        self.fallback_importer(self.cfd_model_objects)
---
+++ backend/app/db/importer/carfueldata/cfd_reader.py
@@ -0,0 +1,73 @@
+import csv
+import logging
+import time
+from enum import Enum
+
+from app.db.importer.base_reader import BaseReader
+from app.db.importer.carfueldata.cfd_objects import CFDImportCar
+from app.db.importer.mappings import CFDHeaderMapping
+from app.misc import file_management
+# LatestCars
+from app.misc.data_handling import check_manufacturer
+
+logger = logging.getLogger(__name__)
+
+
+class CarFuelDataReader(BaseReader):
+    def __init__(self):
+        super().__init__()
+        self.cfd_objects_list = list()
+
+    def _process_data(self, zip_file):
+        files: [] = file_management.unzip_download(
+            zip_file_path=zip_file, destination_folder=self.tempfolder
+        )
+        for cs_file in files:
+            if cs_file.rsplit(".", 1)[-1] == "csv":
+                with open(cs_file, encoding="cp1252") as f:
+                    reader = csv.reader(f, dialect="excel")
+                    header_row = reader.__next__()
+                    headers: dict = {}
+                    counter = 0
+                    for h in header_row:
+                        mapping = CFDHeaderMapping.from_value(h)
+                        if mapping is not None:
+                            headers[mapping] = counter
+                        counter += 1
+                    for row in reader:
+                        manufacturer = row[headers[CFDHeaderMapping.MANUFACTURER]]
+                        real_manufacturer = check_manufacturer(
+                            manufacturer_to_check=manufacturer
+                        )
+                        if not real_manufacturer or not len(real_manufacturer):
+                            continue
+                        cfd_object = CFDImportCar()
+                        setattr(
+                            cfd_object,
+                            CFDHeaderMapping.MANUFACTURER.name.lower(),
+                            real_manufacturer,
+                        )
+                        key: Enum
+                        value: int
+                        for key, value in headers.items():
+                            setattr(cfd_object, key.value.casefold(), row[value])
+                        self.cfd_objects_list.append(cfd_object)
+
+    def _fetch_data(self):
+        # Todo reanable after development
+        # data_file = file_management.download_file_with_name(
+        #     url=settings.CARFUELDATA_URL,
+        #     file_name="latest_cfd_data.zip",
+        #     output_folder=self.tempfolder,
+        # )
+        data_file = "/home/jules/workspace/HeiGIT/openfuelservice/backend/files/Euro_6_latest_22-12-2021.zip"
+        self._process_data(zip_file=data_file)
+
+    def fetch_data(self) -> []:
+        start_time = time.time()
+        print("Crawl Latest Car Fuel Data Objects")
+        self._fetch_data()
+        end_time = time.time()
+        logger.info(
+            f"Crawl-Result: {self.cfd_objects_list.__sizeof__()} Car Fuel Data Objects | {end_time - start_time} Seconds"
+        )
