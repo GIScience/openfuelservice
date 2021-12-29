fb9a6b128846ed683c985dd586c96ab26b587906
Julian Psotta <julianpsotta@gmail.com>
2021-12-29 14:51:55 +0100

    remove hash id and add a class getter function with filter

--- backend/app/db/base_class.py
+++ backend/app/db/base_class.py
@@ -1,14 +1,21 @@
 from typing import Any
 
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.orm import Session
 
 
 @as_declarative()
 class Base:
     id: Any
+    #  hash_id = Column(CHAR(length=32), primary_key=True)
+
     __name__: str
 
     # Generate __tablename__ automatically
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
+
+    @classmethod
+    def get_all_by_filter(cls, db: Session, filter_ids: []):
+        return cls.query().filter(cls.id.in_(filter_ids)).all()
