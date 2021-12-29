34b5c0976a081dfcc59258b6b7a4ad343aeab987
Julian Psotta <julianpsotta@gmail.com>
2021-12-29 14:48:16 +0100

    add method to get the correct brands from file

---
+++ backend/app/tests/misc/test_data_handling.py
@@ -0,0 +1,20 @@
+import pytest
+
+from app.misc.data_handling import check_manufacturer
+
+
+@pytest.mark.parametrize(
+    "bare_manufacturer,real_manufacturer",
+    (
+        ("3 GE", "3 GE"),
+        ("ACAM", "ACAM"),
+        ("Opel", "Opel"),
+        ("azlk", "Moskvitch"),
+        ("PSA / Fiat", "Fiat"),
+        ("Foobar", None),
+    ),
+)
+def test_check_manufacturer(bare_manufacturer: str, real_manufacturer: str):
+    assert real_manufacturer == check_manufacturer(
+        manufacturer_to_check=bare_manufacturer
+    )
