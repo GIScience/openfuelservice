d71a5c53c80678ad00cfb77ea21d892ddd3b9ad5
Julian Psotta <julianpsotta@gmail.com>
2021-12-30 11:08:36 +0100

    differentiate between fail and non-fail tests

--- backend/app/tests/misc/test_filemanagement.py
+++ backend/app/tests/misc/test_filemanagement.py
@@ -1,6 +1,7 @@
 from os.path import basename
 from pathlib import Path
 from typing import Any, List
+from zipfile import BadZipFile
 
 import pytest
 
@@ -9,33 +10,49 @@ from app.misc.file_management import download_file_with_name, unzip_download
 
 
 @pytest.mark.parametrize(
-    "source_file,dest_file,return_type",
+    "source_file,dest_file",
     (
-        (settings.CARFUELDATA_TEST_PATH_OR_URL, "test1_output.zip", Path),
-        (settings.CARFUELDATA_TEST_PATH_OR_URL, "test2_output.zip", Path),
-        ("Foobar", "test3_output.zip", FileNotFoundError),
+        (settings.CARFUELDATA_TEST_PATH_OR_URL, "test1_output.zip"),
+        (settings.CARFUELDATA_TEST_PATH_OR_URL, "test2_output.zip"),
     ),
 )
 def test_download_file_with_name(
-    source_file: str, dest_file: str, return_type: Any, tmpdir: Path
+    source_file: str, dest_file: str, tmpdir: Path
 ) -> None:
-    test: Path | FileNotFoundError = download_file_with_name(
-        source_file, dest_file, tmpdir
-    )
-    assert isinstance(test, return_type)
-    if not isinstance(test, FileNotFoundError):
-        assert basename(test) == dest_file
-        assert test.exists()
+    test: Path = download_file_with_name(source_file, dest_file, tmpdir)
+    assert isinstance(test, Path)
+    assert basename(test) == dest_file
+    assert test.exists()
+
+
+@pytest.mark.parametrize(
+    "source_file,dest_file",
+    (("Foobar1", "foobar1_output.zip"), ("Foobar2", "foobar2_output.zip"),),
+)
+def test_download_file_with_name_must_fail(
+    source_file: str, dest_file: str, tmpdir: Path
+) -> None:
+    with pytest.raises(FileNotFoundError):
+        download_file_with_name(source_file, dest_file, tmpdir)
 
 
 @pytest.mark.parametrize(
     "source_file,return_values",
-    ((settings.CARFUELDATA_TEST_PATH_OR_URL, ["Euro_6_latest.csv"]), ("Foobar", []),),
+    ((settings.CARFUELDATA_TEST_PATH_OR_URL, ["Euro_6_latest.csv"]),),
 )
 def test_unzip_download(source_file: str, return_values: List, tmpdir: Path) -> None:
     test: List = unzip_download(Path(source_file), Path(tmpdir))
-    if not len(test):
-        assert len(test) == len(return_values)
-    else:
-        for return_value in return_values:
-            assert Path(f"{tmpdir}/{return_value}").exists()
+    for file in test:
+        assert basename(file) in return_values
+    for return_value in return_values:
+        assert Path(f"{tmpdir}/{return_value}").exists()
+
+
+@pytest.mark.parametrize(
+    "source_file, exception", (("Foobar.zip", BadZipFile), ("Foobar", BadZipFile)),
+)
+def test_unzip_download_must_fail(
+    source_file: str, exception: Any, tmpdir: Path
+) -> None:
+    with pytest.raises(exception):
+        unzip_download(Path(source_file), Path(tmpdir))
