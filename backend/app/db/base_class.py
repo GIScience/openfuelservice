f590e9a27843fa1c059de24e2f5ea6ec1bbb4f1d
Julian Psotta <julianpsotta@gmail.com>
2022-01-01 23:36:37 +0100

    move set_data to the base class

--- backend/app/db/base_class.py
+++ backend/app/db/base_class.py
@@ -1,7 +1,9 @@
-from typing import Any
+from typing import Any, Dict, List
 
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 from sqlalchemy.orm import Session
+
+from app.db.importer.mappings import BaseMapping
 
 
 @as_declarative()
@@ -18,3 +20,20 @@ class Base:
     @classmethod
     def get_all_by_filter(cls, db: Session, filter_ids: list) -> list:
         return cls.query().filter(cls.id.in_(filter_ids)).all()  # type: ignore
+
+    def set_data(self, data: List, headers: Dict) -> None:
+        value: Any
+        key: BaseMapping
+        for key, header_index in headers.items():
+            value = data[header_index]
+            if value is None or str(value).lower().strip() == "n/a" or key is None:
+                value = None
+            elif type(value) == str and not len(value):
+                value = None
+            elif type(value) == str and "." in value and value.isdigit():
+                value = float(value)
+            elif type(value) == str and value.isdigit():
+                value = int(value)
+            elif type(value) == str and value.lower() in ["true", "yes", "no", "false"]:
+                value = bool(value)
+            self.__setattr__(key.name.lower(), value)
