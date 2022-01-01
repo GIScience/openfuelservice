de9e221a1241ba69f68eccb5f13e14b285d61874
Julian Psotta <julianpsotta@gmail.com>
2022-01-01 23:37:28 +0100

    add test for base db class

---
+++ backend/app/tests/db/test_base_class.py
@@ -0,0 +1,9 @@
+from app.db.base_class import Base
+from app.db.importer.mappings import CountriesMapping
+
+
+def test_base_class():
+    test_base = Base()
+    test_base.set_data([22], {CountriesMapping.NUMERIC: 0})
+    assert test_base.numeric == 22
+    assert len(test_base.__dict__) == 1
