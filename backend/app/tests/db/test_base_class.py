dfee842c1aef421570f5087bf831a12089423075
Julian Psotta <julianpsotta@gmail.com>
2022-01-02 21:59:35 +0100

    change to a more linting friendly dict access

--- backend/app/tests/db/test_base_class.py
+++ backend/app/tests/db/test_base_class.py
@@ -2,8 +2,8 @@ from app.db.base_class import Base
 from app.db.importer.mappings import CountriesMapping
 
 
-def test_base_class():
+def test_base_class() -> None:
     test_base = Base()
     test_base.set_data([22], {CountriesMapping.COUNTRY_NUMERIC: 0})
-    assert test_base.country_numeric == 22
+    assert test_base.__dict__["country_numeric"] == 22
     assert len(test_base.__dict__) == 1
