39bd829e509f9e5b0814ea4e7cea81bc68a903f8
Julian Psotta <julianpsotta@gmail.com>
2022-01-08 19:07:33 +0100

    add base hashing method

--- backend/app/db/base_class.py
+++ backend/app/db/base_class.py
@@ -1,6 +1,9 @@
+import datetime
+import hashlib
 from typing import Any, Dict, List
 
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import Session
 
 from app.db.importer.mappings import BaseMapping
@@ -42,3 +45,18 @@ class Base:
             elif type(value) == str and value.lower() in ["true", "false"]:
                 value = bool(value)
             self.__setattr__(key.name.lower(), value)
+
+    def hash_object(self) -> str:
+        hash_string: str = ""
+
+        primary_keys = [key.name for key in inspect(self.__class__).primary_key]
+
+        key: str
+        for key in primary_keys:
+            if key.startswith("_") or key not in self.__dict__.keys():
+                continue
+            value: Any = self.__dict__.get(key)
+            if isinstance(value, datetime.datetime) or isinstance(value, datetime.date):
+                value = value.strftime("%Y%m%d")
+            hash_string = f"{hash_string}{str(value).strip()}"
+        return hashlib.md5(hash_string.strip(" ").encode("utf-8")).hexdigest()
