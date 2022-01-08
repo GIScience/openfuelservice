43057e8f99bf22aefce69ec75ea5f8d2d1985afd
Julian Psotta <julianpsotta@gmail.com>
2022-01-08 19:06:03 +0100

    make the importer even more generic to be able to import all objects

--- backend/app/db/importer/base_importer.py
+++ backend/app/db/importer/base_importer.py
@@ -1,14 +1,12 @@
 import logging
-from typing import Dict, List, Union
+from typing import Dict, List
 
 from sqlalchemy import exc
 from sqlalchemy.orm import Session
 from sqlalchemy.orm.exc import FlushError
 from tqdm import tqdm
 
-from app.db.importer.carfueldata.cfd_reader import CarFuelDataReader
-from app.db.importer.countries.countries_reader import CountryCodesReader
-from app.models import CarFuelDataCar, CountryData
+from app.db.base_class import Base
 
 logger = logging.getLogger(__name__)
 
@@ -19,17 +17,14 @@ class BaseImporter:
         self._int_hashes: Dict = {}
         self._object_collection: List = []
 
-    def import_data(
-        self, reader_object: Union[CarFuelDataReader, CountryCodesReader]
-    ) -> None:
+    def import_data(self, db_objects: List[Base]) -> None:
         for database_object in tqdm(
-            reader_object.objects_list,
-            total=len(reader_object.objects_list),
-            unit=f"Importing Database Objects for class {self.__class__}",
+            db_objects,
+            total=len(db_objects),
+            unit=" DB Objects",
+            desc=" Importing Database Objects.",
         ):
-            if type(database_object) == CarFuelDataCar:
-                self._object_collection.append(database_object)
-            elif type(database_object) == CountryData:
+            if issubclass(type(database_object), Base):
                 self._object_collection.append(database_object)
         self._fallback_importer()
 
@@ -40,29 +35,32 @@ class BaseImporter:
 
         :param merge_on_duplicate: Set to ture to merge existing database objects.
         """
-        counter = 0
-        db_object_hashes: set = set()
+        # Check per object class for duplicate objects
+        db_object_hashes: Dict = {}
         for i in range(len(self._object_collection) - 1, -1, -1):
-            db_object: CarFuelDataCar = self._object_collection[i]
-            if not db_object.__dict__.__contains__("id"):
-                continue
-            elif db_object.id not in db_object_hashes:
-                db_object_hashes.add(db_object.id)
+            db_object: Base = self._object_collection[i]
+            if str(type(db_object)) not in db_object_hashes:
+                db_object_hashes[str(type(db_object))] = set()
+            object_hash: str = db_object.hash_object()
+            if object_hash not in db_object_hashes[str(type(db_object))]:
+                db_object_hashes[str(type(db_object))].add(object_hash)
             else:
                 self._object_collection.pop(i)
-                counter += 1
-                continue
-            if merge_on_duplicate and self._object_collection[i].get_all_by_filter(
-                self.db, [self._object_collection[i].id]
-            ):
-                self.db.merge(self._object_collection.pop(i))
-
         try:
             self.db.add_all(self._object_collection)
             self.db.commit()
         except exc.IntegrityError as err:
             logger.warning(f"Integrity error with message: {err}")
             self.db.rollback()
+            for i in range(len(self._object_collection) - 1, -1, -1):
+                existing_objects: List = self._object_collection[i].get_all_by_filter(
+                    self.db, [self._object_collection[i].id]
+                )
+                if merge_on_duplicate and len(existing_objects):
+                    self.db.merge(self._object_collection.pop(i))
+                elif len(existing_objects):
+                    self._object_collection.pop(i)
+            self._fallback_importer(merge_on_duplicate=merge_on_duplicate)
         except TypeError as err:
             logger.warning(f"Type error with message: {err}")
             self.db.rollback()
