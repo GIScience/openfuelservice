13ebfb6e55512c15f97d109c9ef0a796b4c21ab0
Julian Psotta <julianpsotta@gmail.com>
2022-01-08 19:10:07 +0100

    import the new sqlalchemy models for the fuel prices

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -3,6 +3,8 @@
 from app.db.base_class import Base  # noqa
 from app.models.carfueldata import CarFuelDataAverageCategoryStatistics  # noqa
 from app.models.carfueldata import CarFuelDataCar  # noqa
+from app.models.eurostat import EurostatCountryPrice  # noqa
+from app.models.eurostat import EurostatGeneralPrice  # noqa
 from app.models.item import Item  # noqa
 from app.models.user import User  # noqa
 from app.models.wikipedia import WikiCar  # noqa
