da49d6988a030f768bd510a84bb74c94805c1cc0
Julian Psotta <julianpsotta@gmail.com>
2022-02-17 13:14:55 +0100

    correct brands endpoint

--- backend/app/api/api_v1/endpoints/brands.py
+++ backend/app/api/api_v1/endpoints/brands.py
@@ -1,6 +1,6 @@
-from typing import Any
+from typing import Any, Dict, List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app import schemas
@@ -18,3 +18,58 @@ async def read_brands(db: Session = Depends(deps.get_db)) -> Any:
             value for (value,) in db.query(CarFuelDataCar.manufacturer).distinct().all()
         ]
     }
+
+
+@router.get(
+    "/{brand}/cars", response_model=schemas.Cars, description="Return cars by brand."
+)
+async def read_cars_by_brand(brand: str, db: Session = Depends(deps.get_db)) -> Any:
+    """Request the available models for fuel calculations."""
+    results = (
+        db.query(CarFuelDataCar).filter(CarFuelDataCar.manufacturer.like(brand)).all()
+    )
+    if not results:
+        raise HTTPException(status_code=404, detail=f"No cars found for brand: {brand}")
+    return {"data": results}
+
+
+@router.get(
+    "/{brand}/cars/sorted",
+    response_model=schemas.CarsSorted,
+    description="Return cars by brand ordered after Model and year.",
+)
+async def read_cars_by_brand_sorted_by_model_and_year(
+    brand: str, db: Session = Depends(deps.get_db)
+) -> Any:
+    """Request the available models for fuel calculations."""
+    results: Dict = {"ids": []}
+    database_entries: List = db.query(CarFuelDataCar).filter(
+        CarFuelDataCar.manufacturer.like(brand)
+    ).all()
+    if not database_entries:
+        raise HTTPException(status_code=404, detail=f"No cars found for brand: {brand}")
+    entry: CarFuelDataCar
+    for entry in database_entries:
+        database_entries.remove(entry)
+        if entry.model not in results:
+            results[entry.model] = {}
+            results[entry.model]["ids"] = []
+        if entry.year not in results[entry.model]:
+            results[entry.model][entry.year] = {}
+            results[entry.model][entry.year]["cars"] = []
+            results[entry.model][entry.year]["ids"] = []
+        results[entry.model][entry.year]["cars"].append(
+            {
+                "id": entry.id,
+                "manufacturer": entry.manufacturer,
+                "model": entry.model,
+                "description": entry.description,
+                "fuel_type": entry.fuel_type,
+                "year": entry.year,
+            }
+        )
+        results["ids"].append(entry.id)
+        results[entry.model]["ids"].append(entry.id)
+        results[entry.model][entry.year]["ids"].append(entry.id)
+
+    return {"data": results}
--- backend/app/tests/api/api_v1/test_brands.py
+++ backend/app/tests/api/api_v1/test_brands.py
@@ -1,4 +1,4 @@
-from typing import Generator, List
+from typing import Dict, Generator, List, Union
 
 import pytest
 from httpx import AsyncClient
@@ -6,6 +6,7 @@ from sqlalchemy.orm import Session
 
 from app.core.config import settings
 from app.models import CarFuelDataCar
+from app.schemas import Car
 
 
 @pytest.mark.anyio
@@ -36,3 +37,83 @@ async def test_read_brands(
             "Hyundai",
         ].sort()
     )
+
+
+@pytest.mark.parametrize(
+    "brand,response_code", (("Abarth", 200), ("", 404), ("Foo", 404), (None, 404),),
+)
+@pytest.mark.anyio
+async def test_read_cars_by_brand(
+    async_client: AsyncClient,
+    db: Session,
+    mock_cfd_cars: Generator,
+    brand: Union[Dict, None],
+    response_code: int,
+) -> None:
+    response = await async_client.get(f"{settings.API_V1_STR}/brands/{brand}/cars")
+    assert response.status_code == response_code
+    if response.status_code == 200:
+        content: dict = response.json()
+        assert isinstance(content["data"], list)
+        data = content["data"]
+        assert len(data) == 5
+        ids_in_mocks: List = [cfd.id for cfd in mock_cfd_cars]
+        car: Dict
+        for car in data:
+            orm_car = Car.parse_obj(car)
+            assert orm_car.id in ids_in_mocks
+            assert orm_car.manufacturer
+            assert orm_car.model
+            assert orm_car.description
+            assert orm_car.fuel_type
+            assert orm_car.year
+
+
+@pytest.mark.parametrize(
+    "brand,response_code", (("Abarth", 200), ("", 404), ("Foo", 404), (None, 404),),
+)
+@pytest.mark.anyio
+async def test_cars_by_brand_sorted_by_model_and_year(
+    async_client: AsyncClient,
+    db: Session,
+    mock_cfd_cars: Generator,
+    brand: Union[Dict, None],
+    response_code: int,
+) -> None:
+    response = await async_client.get(
+        f"{settings.API_V1_STR}/brands/{brand}/cars/sorted"
+    )
+    assert response.status_code == response_code
+    ids = [
+        "58a30c268922539271bd781aa3c059c4",
+        "30f025757624185e6025ade861756d16",
+        "4d67f0605929e5e6c9a4f4fbb6ef99a6",
+    ]
+    if response.status_code == 200:
+        content: dict = response.json()
+        assert len(content) == 2
+        assert isinstance(content["messages"], list)
+        assert isinstance(content["data"], dict)
+        data = content["data"]
+        assert len(data) == 2
+        assert len(data["ids"]) == 3
+        assert data["ids"] == ids
+        car_name = data["595"]
+        assert len(car_name) == 2
+        assert len(car_name["ids"]) == 3
+        assert car_name["ids"] == ids
+        car_name_2021 = car_name["2021"]
+        assert len(car_name_2021) == 2
+        assert len(car_name_2021["ids"]) == 3
+        assert car_name_2021["ids"] == ids
+        cars = car_name_2021["cars"]
+        assert len(cars) == 3
+        car: Dict
+        for car in cars:
+            orm_car = Car.parse_obj(car)
+            assert orm_car.id in ids
+            assert orm_car.manufacturer
+            assert orm_car.model
+            assert orm_car.description
+            assert orm_car.fuel_type
+            assert orm_car.year
