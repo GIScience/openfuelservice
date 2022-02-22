66e1c50979476d886a37cfef7d2a6e46f82fa5f3
Julian Psotta <julianpsotta@gmail.com>
2022-02-22 16:30:53 +0100

    change response of get wikicar_by_name to get only single entries or none.

--- backend/app/tests/matcher/test_base_matcher.py
+++ backend/app/tests/matcher/test_base_matcher.py
@@ -1,5 +1,6 @@
 from typing import Generator, List, Union
 
+import pytest
 from sqlalchemy.orm import Session
 
 from app.core.config import settings
@@ -8,7 +9,7 @@ from app.models import WikiCar
 
 
 def test_base_matcher_initialization(
-    db: Session, mock_wikipedia_cars: Generator[WikiCar, None, None]
+        db: Session, mock_wikipedia_cars: Generator[WikiCar, None, None]
 ) -> None:
     base_matcher: BaseMatcher = BaseMatcher(  # type: ignore
         models_path=settings.UNCOMPRESSED_MATCHING_DATA, db=db
@@ -16,8 +17,8 @@ def test_base_matcher_initialization(
     assert base_matcher._db == db
 
 
-def test_base_matcher_get_wikicars_by_name(
-    db: Session, mock_wikipedia_cars: Generator[WikiCar, None, None]
+def test_base_matcher_get_wikicar_by_name(
+        db: Session, mock_wikipedia_cars: Generator[WikiCar, None, None]
 ) -> None:
     base_matcher: BaseMatcher = BaseMatcher(  # type: ignore
         models_path=settings.UNCOMPRESSED_MATCHING_DATA, db=db
@@ -27,15 +28,27 @@ def test_base_matcher_get_wikicars_by_name(
     mock_wikipedia_car: WikiCar
     for mock_wikipedia_car in mock_wikipedia_cars:
         assert isinstance(mock_wikipedia_car, WikiCar)
-        wiki_cars_by_name: Union[
-            List[WikiCar], None
-        ] = base_matcher.get_wikicars_by_name(mock_wikipedia_car.wiki_name)
-        assert wiki_cars_by_name and len(wiki_cars_by_name) > 0
-        assert all(
-            isinstance(wiki_car_by_name, WikiCar)
-            for wiki_car_by_name in wiki_cars_by_name
-        )
-        assert all(
-            wiki_car_by_name.wiki_name == mock_wikipedia_car.wiki_name
-            for wiki_car_by_name in wiki_cars_by_name
-        )
+        wiki_car_by_name: Union[WikiCar, None] = base_matcher.get_wikicar_by_name(mock_wikipedia_car.wiki_name)
+        assert wiki_car_by_name
+        assert isinstance(wiki_car_by_name, WikiCar)
+        assert wiki_car_by_name.wiki_name == mock_wikipedia_car.wiki_name
+
+
+@pytest.mark.parametrize(
+    "wiki_name,expected_result",
+    (
+            ("Foo Foo", None),
+            ("", None),
+            (None, None),
+    ),
+)
+def test_base_matcher_get_wikicar_by_name_is_empty(
+        db: Session,
+        wiki_name: str,
+        expected_result: Union[str, None]
+) -> None:
+    base_matcher: BaseMatcher = BaseMatcher(  # type: ignore
+        models_path=settings.UNCOMPRESSED_MATCHING_DATA, db=db
+    )
+    test: Union[WikiCar, None] = base_matcher.get_wikicar_by_name(wiki_name)
+    assert test == expected_result
