0e7a33a989ae3feef5bd3234fb682d8296983096
Julian Psotta <julianpsotta@gmail.com>
2022-02-28 12:26:29 +0100

    add imports for the new wikicarenvirocar

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -3,10 +3,12 @@
 from app.db.base_class import Base  # noqa
 from app.models.carfueldata import CarFuelDataAverageCategoryStatistics  # noqa
 from app.models.carfueldata import CarFuelDataCar  # noqa
+from app.models.envirocar import EnvirocarSensor  # noqa
 from app.models.eurostat import EurostatCountryPrice  # noqa
 from app.models.eurostat import EurostatGeneralPrice  # noqa
 from app.models.item import Item  # noqa
 from app.models.user import User  # noqa
 from app.models.wikipedia import WikiCar  # noqa
 from app.models.wikipedia import WikiCarCategory  # noqa
+from app.models.wikipedia import WikicarEnvirocar  # noqa
 from app.models.wikipedia import WikiCarPageText  # noqa
